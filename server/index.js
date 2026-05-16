d9a92ec425542fc8d636191a1cdff69e3834156d
vineet2825 <vineetvijay0p@gmail.com>
2026-05-16 13:45:20 +0530

    Fix: Update wildcard route for Express 5 compatibility

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ app.use('/api/upload', require('./routes/uploadRoutes'));
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
-    app.get('*', (req, res) =>
+    app.get('/*', (req, res) =>
         res.sendFile(path.resolve(__dirname, '../', 'client', 'dist', 'index.html'))
     );
 } else {
